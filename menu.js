9c649d35b0ee10891f7cde04c55b058b8db93205
hongkhaidragon <87056289+hongkhaidragon@users.noreply.github.com>
2026-04-03 17:52:26 +0700

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,26 @@
+console.log("MENU.JS LOADED");
+
+// timestamp theo phút (đủ để phá cache, không spam request)
+const now = new Date();
+const v =
+  now.getFullYear().toString() +
+  (now.getMonth() + 1) +
+  now.getDate() +
+  now.getHours() +
+  Math.floor(now.getMinutes() / 5); // 👈 đổi mỗi 5 phút
+
+const MENU_URL =
+  "https://phamkhanh296hk-sudo.github.io/FlashSale-All/assets/menu.html?v=" + v;
+
+fetch(MENU_URL, { cache: "no-store" })
+  .then(res => {
+    console.log("MENU FETCH:", res.status, MENU_URL);
+    return res.text();
+  })
+  .then(html => {
+    document.body.insertAdjacentHTML("beforeend", html);
+    console.log("MENU INSERTED");
+  })
+  .catch(err => {
+    console.error("MENU ERROR:", err);
+  });
